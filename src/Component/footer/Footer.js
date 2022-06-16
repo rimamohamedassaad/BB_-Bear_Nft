c16496db50d159f5dc2a4ab9a5e2eda037ddbf3c
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-06-16 23:53:58 +0300

    done

--- src/Component/footer/Footer.js
+++ src/Component/footer/Footer.js
@@ -3,13 +3,15 @@ import './footer.css'
 import discord from '../../images/Vector.png'
 import facebook from '../../images/XMLID_835_.png'
 import twiter from '../../images/XMLID_827_.png'
+import insta from '../../images/Frame 39678.png'
+
 // import '../navbar.css'
 export const Footer = () => {
     return (
         <div className='footerContainer'>
              <div className='linkContentFotter'>
                     <a href='#twiter' className='socialMediaF'><img src={twiter} alt='twiter' /> </a>
-                    <a href='#facebook' className='socialMediaF'><img src={facebook} alt='insta' /> </a>
+                    <a href='#facebook' className='socialMediaF'><img src={insta} alt='insta' /> </a>
                     <a href='#discord' className='socialMediaF'><img src={discord} alt='discord' /> </a>
                 </div>
                 <div className='footerText'>
